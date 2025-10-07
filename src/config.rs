1fd79e2c09a7f436b103b32413746d1cbb043645
arkboii <ahamedrumi16+++2@gmail.com>
2025-10-07 21:14:07 +0530

    font(config.rs): change from jetbrains nerd font to fontconfig monospace

--- src/config.rs
+++ src/config.rs
@@ -9,7 +9,7 @@ use x11rb::protocol::xproto::KeyButMask;
 pub const BORDER_WIDTH: u32 = 2;
 pub const BORDER_FOCUSED: u32 = 0x6dade3;
 pub const BORDER_UNFOCUSED: u32 = 0xbbbbbb;
-pub const FONT: &str = "JetBrainsMono Nerd Font:style=Bold:size=10";
+pub const FONT: &str = "monospace:size=10";
 
 // ========================================
 // GAPS (Vanity Gaps)
