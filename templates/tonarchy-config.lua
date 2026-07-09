afc403f33e4882f3007fb2d5aead402c1dba7ebd
tonybanters <tonybanters@gmail.com>
2026-07-08 21:58:48 -0700

    updated tonarchy config

--- templates/tonarchy-config.lua
+++ templates/tonarchy-config.lua
@@ -1,25 +1,26 @@
 ---@meta
 ---@module 'oxwm'
-
+---
 local modkey = "Mod4"
 local terminal = "alacritty"
 
 local colors = {
-    fg = "#bbbbbb",
-    red = "#f7768e",
-    bg = "#1a1b26",
-    cyan = "#0db9d7",
-    green = "#9ece6a",
-    lavender = "#a9b1d6",
-    light_blue = "#7aa2f7",
-    grey = "#bbbbbb",
-    blue = "#6dade3",
-    purple = "#ad8ee6",
-    sep = "#444b6a",
+    bg = "#000000",
+    fg = "#d4d4d4",
+    red = "#f14c4c",
+    cyan = "#9cdcfe",
+    green = "#6a9955",
+    light_blue = "#4ec9b0",
+    blue = "#569cd6",
+    purple = "#c586c0",
+    yellow = "#dcdcaa",
+    orange = "#ce9178",
+    grey = "#808080",
+    sep = "#3c3c3c",
 }
 
-
-local tags = { "", "󰊯", "", "", "󰙯", "󱇤", "", "󱘶", "󰧮" } -- Example of nerd font icon tags
+local tags = { "1", "2", "3", "4", "5", "6", "7", "8", "9" }
+-- local tags = { "", "󰊯", "", "󰰏", "󰟿", "󱇤", "", "󱘶", "󰧮" } -- Example of nerd font icon tags
 
 local bar_font = "JetBrainsMono Nerd Font Propo:style=Bold:size=12"
 
@@ -32,7 +33,7 @@ local blocks = {
         underline = true,
     }),
     oxwm.bar.block.static({
-        text = " │  ",
+        text = "│",
         interval = 999999999,
         color = colors.sep,
         underline = false,
@@ -44,7 +45,7 @@ local blocks = {
         underline = true,
     }),
     oxwm.bar.block.static({
-        text = " │  ",
+        text = "│",
         interval = 999999999,
         color = colors.sep,
         underline = false,
@@ -56,53 +57,91 @@ local blocks = {
         color = colors.cyan,
         underline = true,
     }),
+    oxwm.bar.block.static({
+        text = "│",
+        interval = 999999999,
+        color = colors.sep,
+        underline = false,
+    }),
+    oxwm.bar.block.systray({
+    }),
+    -- Uncomment to add battery status (useful for laptops)
+    -- oxwm.bar.block.battery({
+    --     format = "Bat: {}%",
+    --     charging = "⚡ Bat: {}%",
+    --     discharging = "- Bat: {}%",
+    --     full = "✓ Bat: {}%",
+    --     interval = 30,
+    --     color = colors.green,
+    --     underline = true,
+    -- }),
 };
 
+-------------------------------------------------------------------------------
+-- Basic Settings
+-------------------------------------------------------------------------------
 oxwm.set_terminal(terminal)
 oxwm.set_modkey(modkey)
 oxwm.set_tags(tags)
 
+-------------------------------------------------------------------------------
+-- Layouts
+-------------------------------------------------------------------------------
 oxwm.set_layout_symbol("tiling", "[T]")
 oxwm.set_layout_symbol("normie", "[F]")
 oxwm.set_layout_symbol("tabbed", "[=]")
 
+-------------------------------------------------------------------------------
+-- Appearance
+-------------------------------------------------------------------------------
 oxwm.border.set_width(0)
 oxwm.border.set_focused_color(colors.purple)
 oxwm.border.set_unfocused_color(colors.grey)
 
+-- Smart Enabled = No border if 1 window
 oxwm.gaps.set_smart(false)
-oxwm.gaps.set_inner(0, 0)
-oxwm.gaps.set_outer(0, 0)
+oxwm.gaps.set_inner(5, 5)
+oxwm.gaps.set_outer(5, 5)
+
 oxwm.rule.add({ instance = "gimp", floating = true })
 oxwm.rule.add({ class = "firefox", tag = 3 })
 
 oxwm.bar.set_font(bar_font)
 oxwm.bar.set_blocks(blocks)
 oxwm.bar.set_scheme_normal(colors.fg, colors.bg, "#444444")
-oxwm.bar.set_scheme_occupied(colors.cyan, colors.bg, colors.cyan)
-oxwm.bar.set_scheme_selected(colors.cyan, colors.bg, colors.purple)
+oxwm.bar.set_scheme_occupied(colors.blue, colors.bg, colors.cyan)
+oxwm.bar.set_scheme_selected(colors.blue, colors.bg, colors.purple)
 
 oxwm.key.bind({ modkey }, "Return", oxwm.spawn_terminal())
+-- oxwm.key.bind({ modkey }, "D", oxwm.spawn({ "sh", "-c", "dmenu_run -l 10" }))
 oxwm.key.bind({ modkey }, "D", oxwm.spawn({ "sh", "-c", "rofi -show drun" }))
 oxwm.key.bind({ modkey }, "S", oxwm.spawn({ "sh", "-c", "maim -s | xclip -selection clipboard -t image/png" }))
 oxwm.key.bind({ modkey }, "Q", oxwm.client.kill())
 
 oxwm.key.bind({ modkey, "Shift" }, "Slash", oxwm.show_keybinds())
+
 oxwm.key.bind({ modkey, "Shift" }, "F", oxwm.client.toggle_fullscreen())
 oxwm.key.bind({ modkey, "Shift" }, "Space", oxwm.client.toggle_floating())
+
 oxwm.key.bind({ modkey }, "C", oxwm.layout.set("tiling"))
 oxwm.key.bind({ modkey }, "N", oxwm.layout.cycle())
+
 oxwm.key.bind({ modkey }, "H", oxwm.set_master_factor(-5))
 oxwm.key.bind({ modkey }, "L", oxwm.set_master_factor(5))
 oxwm.key.bind({ modkey }, "I", oxwm.inc_num_master(1))
 oxwm.key.bind({ modkey }, "P", oxwm.inc_num_master(-1))
+
 oxwm.key.bind({ modkey }, "A", oxwm.toggle_gaps())
+
 oxwm.key.bind({ modkey, "Shift" }, "Q", oxwm.quit())
 oxwm.key.bind({ modkey, "Shift" }, "R", oxwm.restart())
+
 oxwm.key.bind({ modkey }, "J", oxwm.client.focus_stack(1))
 oxwm.key.bind({ modkey }, "K", oxwm.client.focus_stack(-1))
+
 oxwm.key.bind({ modkey, "Shift" }, "J", oxwm.client.move_stack(1))
 oxwm.key.bind({ modkey, "Shift" }, "K", oxwm.client.move_stack(-1))
+
 oxwm.key.bind({ modkey }, "Comma", oxwm.monitor.focus(-1))
 oxwm.key.bind({ modkey }, "Period", oxwm.monitor.focus(1))
 oxwm.key.bind({ modkey, "Shift" }, "Comma", oxwm.monitor.tag(-1))
@@ -152,3 +191,30 @@ oxwm.key.chord({
     { { modkey }, "Space" },
     { {},         "T" }
 }, oxwm.spawn_terminal())
+
+oxwm.key.chord({
+    { { modkey }, "F" },
+    { {},         "B" }
+}, oxwm.spawn({ "sh", "-c", "$HOME/repos/dmenu-scripts/bookmarks-dmenu.sh" }))
+
+oxwm.key.chord({
+    { { modkey }, "F" },
+    { {},         "F" }
+}, oxwm.spawn({ "sh", "-c", "$HOME/repos/dmenu-scripts/repos-dmenu.sh" }))
+
+oxwm.key.chord({
+    { { modkey }, "F" },
+    { {},         "O" }
+}, oxwm.spawn({ "sh", "-c", "$HOME/repos/dmenu-scripts/tmux-dmenu.sh" }))
+
+
+-------------------------------------------------------------------------------
+-- Autostart
+-------------------------------------------------------------------------------
+-- Commands to run once when OXWM starts
+-- Uncomment and modify these examples, or add your own
+
+-- oxwm.autostart("picom")
+-- oxwm.autostart("xwallpaper --zoom ~/walls/dune.jpg")
+-- oxwm.autostart("dunst")
+-- oxwm.autostart("nm-applet")
